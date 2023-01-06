33ddde565477ef4bb9090a6ad9592ffbd2b1b64f
Martine O. Jakobsen <martine.o.j@gmail.com>
2023-01-06 15:41:34 +0100

    Make view scrollable

--- app/src/main/java/com/simonsickle/composedbarcodes/NavigationRegistry.kt
+++ app/src/main/java/com/simonsickle/composedbarcodes/NavigationRegistry.kt
@@ -1,11 +1,15 @@
 package com.simonsickle.composedbarcodes
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -36,62 +40,64 @@ fun NavigationRegistry() {
                 })
         }
     ) {
-        NavHost(navController = navController, startDestination = "home") {
-            composable("home") {
-                title.value = "Barcode List"
-                BarcodeList(navController = navController)
-            }
-            composable(BarcodeType.AZTEC.name) {
-                title.value = "Aztec"
-                Aztec()
-            }
-            composable(BarcodeType.CODABAR.name) {
-                title.value = "Codabar"
-                Codabar()
-            }
-            composable(BarcodeType.CODE_128.name) {
-                title.value = "Code128"
-                Code128()
-            }
-            composable(BarcodeType.CODE_39.name) {
-                title.value = "Code39"
-                Code39()
-            }
-            composable(BarcodeType.CODE_93.name) {
-                title.value = "Code93"
-                Code93()
-            }
-            composable(BarcodeType.DATA_MATRIX.name) {
-                title.value = "DataMatrix"
-                DataMatrix()
-            }
-            composable(BarcodeType.EAN_13.name) {
-                title.value = "Ean13"
-                Ean13()
-            }
-            composable(BarcodeType.EAN_8.name) {
-                title.value = "Ean8"
-                Ean8()
-            }
-            composable(BarcodeType.ITF.name) {
-                title.value = "Itf"
-                Itf()
-            }
-            composable(BarcodeType.PDF_417.name) {
-                title.value = "Pdf 417"
-                Pdf417()
-            }
-            composable(BarcodeType.QR_CODE.name) {
-                title.value = "QR Code"
-                Qr()
-            }
-            composable(BarcodeType.UPC_A.name) {
-                title.value = "UPC A"
-                UpcA()
-            }
-            composable(BarcodeType.UPC_E.name) {
-                title.value = "UPC E"
-                UpcE()
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            NavHost(navController = navController, startDestination = "home") {
+                composable("home") {
+                    title.value = "Barcode List"
+                    BarcodeList(navController = navController)
+                }
+                composable(BarcodeType.AZTEC.name) {
+                    title.value = "Aztec"
+                    Aztec()
+                }
+                composable(BarcodeType.CODABAR.name) {
+                    title.value = "Codabar"
+                    Codabar()
+                }
+                composable(BarcodeType.CODE_128.name) {
+                    title.value = "Code128"
+                    Code128()
+                }
+                composable(BarcodeType.CODE_39.name) {
+                    title.value = "Code39"
+                    Code39()
+                }
+                composable(BarcodeType.CODE_93.name) {
+                    title.value = "Code93"
+                    Code93()
+                }
+                composable(BarcodeType.DATA_MATRIX.name) {
+                    title.value = "DataMatrix"
+                    DataMatrix()
+                }
+                composable(BarcodeType.EAN_13.name) {
+                    title.value = "Ean13"
+                    Ean13()
+                }
+                composable(BarcodeType.EAN_8.name) {
+                    title.value = "Ean8"
+                    Ean8()
+                }
+                composable(BarcodeType.ITF.name) {
+                    title.value = "Itf"
+                    Itf()
+                }
+                composable(BarcodeType.PDF_417.name) {
+                    title.value = "Pdf 417"
+                    Pdf417()
+                }
+                composable(BarcodeType.QR_CODE.name) {
+                    title.value = "QR Code"
+                    Qr()
+                }
+                composable(BarcodeType.UPC_A.name) {
+                    title.value = "UPC A"
+                    UpcA()
+                }
+                composable(BarcodeType.UPC_E.name) {
+                    title.value = "UPC E"
+                    UpcE()
+                }
             }
         }
     }
