f9bf03d67681fe95af0f1c8d0db3eced895ee59a
Simon Sickle <51972200+simonsickle@users.noreply.github.com>
2023-03-14 23:46:32 -0400

    Update deps to latest compose

--- composed-barcodes/src/main/kotlin/com/simonsickle/compose/barcodes/Barcode.kt
+++ composed-barcodes/src/main/kotlin/com/simonsickle/compose/barcodes/Barcode.kt
@@ -2,16 +2,16 @@ package com.simonsickle.compose.barcodes
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
